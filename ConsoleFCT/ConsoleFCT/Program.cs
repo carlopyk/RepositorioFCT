ab0c5c9b37e51b0888209cf127f9c63d8448d2a2
carlopyk <alumno.141643@iesazarquiel.es>
2020-06-04 18:07:08 +0200

    primer commit

---
+++ ConsoleFCT/ConsoleFCT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFCT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
